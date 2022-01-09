cbe1ee9bd743d84106a380e04edbd68fb1750407
F3T1W <particular0010abyss@gmail.com>
2022-01-09 03:54:17 +0300

    Elden Ring Update

--- js/main3.js
+++ js/main3.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/01/21';
+        var finalDate = '2022/02/25';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
